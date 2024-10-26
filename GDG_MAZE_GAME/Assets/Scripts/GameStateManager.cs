526395ead6c5dd383f002175a8110791c2ce6489
SebboScruff <36053779+SebboScruff@users.noreply.github.com>
2024-10-26 17:01:17 +0100

    FMOD integration added into GameStateManager
    Adjusts parameters based on pause state and in-game timer. Timer is currently 300s

--- GDG_MAZE_GAME/Assets/Scripts/GameStateManager.cs
+++ GDG_MAZE_GAME/Assets/Scripts/GameStateManager.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
+using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Game State Manager Class, used for storing and transferring important game data
+/// such as current game state (paused vs in puzzle vs in overworld etc), 
+/// </summary>
 public class GameStateManager : MonoBehaviour
 {
+    const int GAME_TIMER_SECONDS = 300; // How long the in-game timer will last before the player loses
+
     // Input Checking
     private bool PressedPause() => Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Tab);
 
+    /// <summary>
+    /// Available Game States. May need to break into individual puzzles but hopefully not.
+    /// </summary>
     [System.Serializable] public enum GameStates
     {
         OVERWORLD,  // When the Player is exploring the Maze.
@@ -15,7 +26,19 @@ public class GameStateManager : MonoBehaviour
     }
     public GameStates currentGameState { get; private set; }
 
+    // GUI Parent Objects
     [SerializeField] GameObject overworldGUI, pauseGUI, doorGUI; // TODO Add other Puzzle Screens to manage GUI activation/deactivation.
+
+    [Header("Music")]
+    // Make sure these are on different objects - things tend to go wrong if one gameobject has more than 1 FMOD event emitter
+    [SerializeField] StudioEventEmitter backgroundMusicEmitter;
+    [SerializeField] StudioEventEmitter pauseSnapshot;
+
+    // Timer Settings
+    private float gameTimerRemaining;
+
+    [Header("Puzzle Setup")]
+
 
     private void Awake()
     {
@@ -25,7 +48,7 @@ public class GameStateManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameTimerRemaining = 0; // start at 0 and increment for easier FMOD use
     }
 
     // Update is called once per frame
@@ -49,6 +72,19 @@ public class GameStateManager : MonoBehaviour
                     break;
             }
         }
+
+        // At the end of Update, decrease the game timer unless paused.
+        if(currentGameState != GameStates.PAUSED)
+        {
+            gameTimerRemaining += Time.deltaTime;
+            if (gameTimerRemaining >= GAME_TIMER_SECONDS)
+            {
+                // Player has lost. Go to Game Over Screen here.
+            }
+        }
+
+        // FMOD PARAMETER UPDATES - Make sure String Parameters are exactly correct. 
+        backgroundMusicEmitter.SetParameter("Time_Elapsed", gameTimerRemaining / GAME_TIMER_SECONDS); // Increments by the inverse of the max timer so that the BGM layers are added correctly.
     }
 
     public void SetGameState(GameStates _newState)
@@ -61,13 +97,16 @@ public class GameStateManager : MonoBehaviour
                 overworldGUI.SetActive(true);
                 pauseGUI.SetActive(false);
                 doorGUI.SetActive(false);
+                pauseSnapshot.Stop();
                 break;
             case GameStates.PUZZLE:
+                pauseSnapshot.Stop();
                 break;
             case GameStates.PAUSED:
                 overworldGUI.SetActive(false);
                 pauseGUI.SetActive(true);
                 doorGUI.SetActive(false);
+                pauseSnapshot.Play();
                 break;
             default:
                 break;
