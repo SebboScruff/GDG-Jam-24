3219444584f79dc644e4c3b1bfd62844ee4e592d
Rene Schwartz <rene.j.schwartz@gmail.com>
2024-10-27 04:02:39 +0200

    Spinning Disc puzzle shader + scene

---
+++ GDG_MAZE_GAME/Assets/Scripts/Puzzles/SpinningDiscsPuzzleManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningDiscsPuzzleManager : MonoBehaviour
+{
+    [SerializeField] GameStateManager gameStateManager;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
